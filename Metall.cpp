519b45d73f57bd1cd53e911f41d9cea37b9d816c
aescurgo <aescurgo@alumnes.ub.edu>
2017-03-23 00:28:32 +0100

    modified Metall scatter method (raig reflectit)

--- Metall.cpp
+++ Metall.cpp
@@ -16,8 +16,14 @@ Metall::~Metall()
 
 bool Metall::scatter(const Ray& r_in, const HitInfo& rec, vec3& color, Ray& scattered) const
 {
-    vec3 target = rec.p + rec.normal + this->RandomInSphere();
-    scattered = Ray(rec.p, target-rec.p);
+    //vec3 target = rec.p + rec.normal + this->RandomInSphere();
+    vec3 distance = r_in.origin + rec.p;
+    vec3 target = distance- (2.0f * (distance * rec.normal) )* rec.normal;
+
+    float ep = 0.01;
+    vec3 p0 = rec.p + (ep * target);//to solve surface acne.. mod the point
+
+    scattered = Ray(p0, target-p0);
     color = this->specular;
-    return true; //KS
+    return true;
 }
