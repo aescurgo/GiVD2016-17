b2e422fc86a74f7c9cdd2e7e305e3d01da12eb78
Alvaro Escurra <aescurra@MacBook-Pro-de-Alvaro.local>
2017-05-31 22:21:28 +0200

    Practica 3 acabada

---
+++ Prac3/read.py
@@ -0,0 +1,65 @@
+import csv
+import sys
+
+f = open(sys.argv[1], 'rt')
+f2 = open('2008.csv', 'rt')
+data = []
+data2 = []#num of distance
+data3 = []#air time
+aiports = {}
+airport_data = {} #to allocate flightnum
+airport_data_distance = {}
+airport_data_airtime = {}
+minX=0.0
+maxX=6689.0
+maxX2= 1989.0
+maxX3= 242.0
+try:
+    reader = csv.reader(f)
+    for row in reader:
+    	if row[4] != '\N':
+        	aiports[row[4]] = [row[6],row[7]]
+
+    reader2 = csv.reader(f2)
+    for r in reader2:
+    	if (r[16] != 'Origin'):
+            airport_data[r[16]] = [r[9]]
+            airport_data_distance[r[16]] = [float(r[18])]
+            if(r[13] !=  "NA"): airport_data_airtime[r[16]]  = [float(r[13])]
+    valor=0
+    for a in aiports:
+        if a in airport_data_distance:
+            nor = (float(airport_data_distance[a][0]) -minX) / (maxX2 - minX)
+            valor =repr(nor)
+        else:
+            valor =repr(0)
+        data2+= (aiports[a][0],aiports[a][1],valor)
+
+    for air in aiports:
+    	if air in airport_data:
+    		nor = (float(airport_data[air][0]) -minX) / (maxX - minX)
+    		valor =repr(nor)
+
+    	else:
+    		valor =repr(0)
+    	data+= (aiports[air][0],aiports[air][1],valor)
+
+    for air in aiports:
+        if air in airport_data_airtime:
+            nor = (float(airport_data_airtime[air][0]) -minX) / (maxX - minX)
+            valor =repr(nor)
+        else:
+            valor =repr(0)
+        data3+= (aiports[air][0],aiports[air][1],valor)
+
+
+    print """
+	[["flight", [%s]],
+    ["distance", [%s]],
+    ["time", [%s]],
+	""" % (",".join(data),
+       ",".join(data2),
+       ",".join(data3))
+	
+finally:
+    f.close()
