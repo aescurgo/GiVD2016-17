bfcc6b53324bc130de06fe80b7475a4625349876
Alvaro Escurra <aescurra@MacBook-Pro-de-Alvaro.local>
2017-05-31 22:35:00 +0200

    comentando la extracion de los datos

--- Prac3/read.py
+++ Prac3/read.py
@@ -1,3 +1,9 @@
+#Autor: Alvaro Escurra
+#Datos necesarios airports.dat y 2008.csv
+
+#NOTA_ Para ejecutar el codigo y guardarlo en un JSON
+# python read.py airports.dat > outputAir.json
+
 import csv
 import sys
 
@@ -16,13 +22,18 @@ maxX2= 1989.0
 maxX3= 242.0
 try:
     reader = csv.reader(f)
+    #recorremos el fichero para obtener todas las coordenadas de los aeropuertos del mundo
+    #Dict --> AIRPORT = {LAT,LON}
     for row in reader:
-    	if row[4] != '\N':
-        	aiports[row[4]] = [row[6],row[7]]
+        if row[4] != '\N':
+            aiports[row[4]] = [row[6],row[7]]
 
     reader2 = csv.reader(f2)
+    #Los 3 bucles es para guardar la informacion de los vuelos, distancia y minutos en el aire
+    #normalizamos los datos
+    #Dict --> AIRPORT = {dataValue}
     for r in reader2:
-    	if (r[16] != 'Origin'):
+        if (r[16] != 'Origin'):
             airport_data[r[16]] = [r[9]]
             airport_data_distance[r[16]] = [float(r[18])]
             if(r[13] !=  "NA"): airport_data_airtime[r[16]]  = [float(r[13])]
@@ -36,13 +47,13 @@ try:
         data2+= (aiports[a][0],aiports[a][1],valor)
 
     for air in aiports:
-    	if air in airport_data:
-    		nor = (float(airport_data[air][0]) -minX) / (maxX - minX)
-    		valor =repr(nor)
+        if air in airport_data:
+            nor = (float(airport_data[air][0]) -minX) / (maxX - minX)
+            valor =repr(nor)
 
-    	else:
-    		valor =repr(0)
-    	data+= (aiports[air][0],aiports[air][1],valor)
+        else:
+            valor =repr(0)
+        data+= (aiports[air][0],aiports[air][1],valor)
 
     for air in aiports:
         if air in airport_data_airtime:
@@ -52,14 +63,15 @@ try:
             valor =repr(0)
         data3+= (aiports[air][0],aiports[air][1],valor)
 
-
+    #Por ultimo hacemos un print, donde nos sacara por pantalla la estructura del JSON necesaria
+    #Para poder utilizarlo
     print """
-	[["flight", [%s]],
+    [["flight", [%s]],
     ["distance", [%s]],
     ["time", [%s]],
-	""" % (",".join(data),
+    """ % (",".join(data),
        ",".join(data2),
        ",".join(data3))
-	
+    
 finally:
     f.close()
